b51f5940bfd8530cf9b6f14bc4a6fed79b96eab6
DressedHuman <dressed.human@gmail.com>
2023-10-26 03:14:58 +0600

    added the firebase auth with google auth provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from 'firebase/auth'
+import app from './firebase/firebase.config'
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const auth = getAuth(app);
+  const providerGoogle = new GoogleAuthProvider();
+  const [user, setUser] = useState(null);
+  
+  const handleSignIn = (provider) => {
+    signInWithPopup(auth, provider)
+    .then(result=>{
+      setUser(result.user);
+      console.log(result.user);
+    })
+    .catch(error => console.log("Error :", error.message))
+  }
+
+  const handleSignOUt = () => {
+    signOut(auth);
+    setUser(null);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style={{display: 'flex', gap: "21px", justifyContent: "center", alignItems: "center"}}>
+      { user ?
+        <div style={{textAlign: 'center'}}>
+          <img src={user.photoURL} alt="" />
+          <h2>Welcome, <span style={{color: 'green'}}>{user.displayName}</span></h2>
+          {
+            user.email && <h3>Email : <small>{user.email}</small></h3>
+          }
+          <button onClick={handleSignOUt} className="button logout">Sign Out</button>
+        </div> :
+        <>
+          <button onClick={() => handleSignIn(providerGoogle)} className='button login'>Signin with Google</button>
+        </>
+      }
+    </div>
   )
 }
 
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyANyNbl0i2Zz1z5GjIHE62XQK1c03SVGxs",
+  authDomain: "dimple-firebase-6bc8c.firebaseapp.com",
+  projectId: "dimple-firebase-6bc8c",
+  storageBucket: "dimple-firebase-6bc8c.appspot.com",
+  messagingSenderId: "819217282830",
+  appId: "1:819217282830:web:c3df476795fe61a045c95f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
